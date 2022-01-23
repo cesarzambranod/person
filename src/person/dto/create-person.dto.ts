d0c9f14e5fd9d8c105dfdaf540087c4bb29d4d5e
cesaritpatagonia <czambrano@itpatagonia.com>
2022-01-23 14:14:17 -0300

    change dto and entities

--- src/person/dto/create-person.dto.ts
+++ src/person/dto/create-person.dto.ts
@@ -3,4 +3,11 @@ export class CreatePersonDto {
   firstname: string;
   lastname: string;
   email: string;
+  phone: string;
+  address: string;
+  status?: number;
+  viewed: boolean;
+  last_address_seen: string;
+  date_created: number;
+  date_updated: number;
 }
--- src/person/entities/person.entity.ts
+++ src/person/entities/person.entity.ts
@@ -4,13 +4,24 @@ import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 export class Person {
   @PrimaryGeneratedColumn()
   id: number;
-
   @Column({ length: 30 })
   firstname: string;
-
   @Column({ length: 30 })
   lastname: string;
-
   @Column({ length: 255 })
   email: string;
+  @Column({ length: 255 })
+  phone: string;
+  @Column({ length: 255 })
+  address: string;
+  @Column({ length: 4 })
+  status: number;
+  @Column()
+  viewed: boolean;
+  @Column({ length: 255 })
+  last_address_seen: string;
+  @Column({ type: 'timestamptz', default: () => 'CURRENT_TIMESTAMP' })
+  date_creted: Date;
+  @Column({ type: 'timestamptz', default: () => 'CURRENT_TIMESTAMP' })
+  date_updated: Date;
 }
